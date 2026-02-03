e8eba99d4b49863b933c03fea9cecfc297d04af5
mathkzm <matheusmiyashiro.mm@gmail.com>
2026-02-03 16:24:33 -0400

    Implementação do arquivo de extração dos dados da API do BCB

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,14 +1,14 @@
-#%%
 import requests
+from datetime import date
 
-def extract_data():
-    url = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='02-02-2026'&$top=100&$format=json&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao"
+def extract_data(data_execucao = None):
+    if data_execucao is None:
+        data_execucao = date.today().strftime("%m-%d-%Y")
+
+    url = f"https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='{data_execucao}'&$top=100&$format=json&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao"
 
     response = requests.get(url)
 
     response.raise_for_status()
 
-    return response.json()
-
-
-
+    return response.json()['value']
