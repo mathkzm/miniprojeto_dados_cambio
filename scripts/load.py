e3517e953b9e0523f1ae288e72933d10dbd89755
mathkzm <matheusmiyashiro.mm@gmail.com>
2026-02-04 10:00:41 -0400

    Adequação de atualização de arquivo CSV

--- scripts/load.py
+++ scripts/load.py
@@ -7,15 +7,9 @@ def load_data_csv(df):
     
     os.makedirs(os.path.dirname(DATA_PATH), exist_ok=True)
 
-    if os.path.exists(DATA_PATH):
-        df_existing = pd.read_csv(DATA_PATH)
-        df_final = pd.concat([df_existing, df], ignore_index=True)
-        df_final = df_final.drop_duplicates(
-            subset=["dataCotacaoDolar"],
-            keep="first"
-        )
-    else:
-        df_final = df
+    df_final = df.drop_duplicates(
+        subset=["dataCotacaoDolar"],
+        keep="first"
+    ).sort_values("dataCotacaoDolar", ascending=True)
 
-    df_final = df_final.sort_values("dataCotacaoDolar", ascending=True)
     df_final.to_csv(DATA_PATH, index=False)
