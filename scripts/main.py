834141490205725062a1c8325d91dc6e7365bd66
mathkzm <matheusmiyashiro.mm@gmail.com>
2026-02-04 10:00:06 -0400

    Adequação de código para extrair a cotação por período de tempo informado pelo usuário

--- scripts/main.py
+++ scripts/main.py
@@ -4,7 +4,12 @@ from extract import extract_data
 from transform import transform_data
 from load import load_data_csv
 
-data = extract_data()
+print("Informe a data no formato MM-DD-YYYY")
+
+data_inicio = input("Informe a data de início: ")
+data_fim = input("Informe a data de fim: ")
+
+data = extract_data(data_inicio,data_fim)
 df = transform_data(data)
 load_data_csv(df)
 
